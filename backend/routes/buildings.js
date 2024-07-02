8fa4a88d38663641c1cd4aef57c502f403a7fc3f
JDemlow <jdemlowdev@gmail.com>
2024-07-02 00:30:26 -0600

    changing file structure

--- backend/routes/buildings.js
+++ backend/routes/buildings.js
@@ -1,6 +1,6 @@
 // routes/buildings.js
 import express from "express";
-import Building from "../../models/building.js";
+import Building from "../models/building.js";
 
 const router = express.Router();
 
